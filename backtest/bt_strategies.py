56b0a60b8aa139322b1d2c7d3e6f90859d883e66
Basant <basant@essenvia.com>
2024-03-21 10:20:31 +0530

    cleanup for prod

--- backtest/bt_strategies.py
+++
@@ -1,7 +0,0 @@
-#from research.strategies.double_top_break_strategy import DoubleTopBreakStrategy
-#from research.strategies.state_cap_strategy import StateCapStrategy
-#from research.strategies.opening_trend_bearish import OpeningBearishTrendStrategy
-from live_algo.friday.friday_candle_first_30_mins import FridayCandleFirst30Buy,FridayCandleFirst30Sell #, #FridayCandleBuyFullDay
-from research.strategies.multi_pattern_q_strategy import MultiPatternQueueStrategy
-from research.technical_strategies.price_below_ema import PriceBreakEMADownward
-from research.technical_strategies.price_reverse_ema_down import PriceReverseBreakDownEMA
--- backtest/settings.py
+++
@@ -1,3 +0,0 @@
-from pathlib import Path
-reports_dir = str(Path(__file__).parent.parent) + '/reports/'
-models_dir = str(Path(__file__).parent.parent) + '/models/'
