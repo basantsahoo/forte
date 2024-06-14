2e34255aaf2d7f6e737850db0fe22e2273dfa889
Basant <basant@essenvia.com>
2024-06-14 13:47:04 +0530

    double top not working

--- strat_machine/strategies/double_top_strategy.py
+++ strat_machine/strategies/double_top_strategy.py
@@ -1,10 +1,11 @@
 from strat_machine.core_strategies.core_strategy import BaseStrategy
-from helper.utils import  get_overlap
+from strat_machine.core_strategies.signal_setup import get_startegy_args
+from helper.utils import get_option_strike
 
 
 class DoubleTopStrategy(BaseStrategy):
     def __init__(self, market_book, **kwargs):
-        BaseStrategy.__init__(self, market_book, **kwargs)
-
+        args = get_startegy_args(**kwargs)
+        BaseStrategy.__init__(self, market_book=market_book, **args)
 
 
